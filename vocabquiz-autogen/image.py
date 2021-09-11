e9670fa7f9505e9c105a81b616c1c5d1b1a1c7f6
JaseprW <jpc72075@gmail.com>
2021-09-11 19:22:08 +0800

    Scanned images and fetch all vocab.

--- vocabquiz-autogen/image.py
+++ vocabquiz-autogen/image.py
@@ -1,4 +1,25 @@
-import cv2
+from PIL import Image
 import pytesseract
+from os import listdir
+from os.path import isfile, isdir, join
 
-img = cv2.imread('image.jpg')
+pytesseract.pytesseract.tesseract_cmd = 'C:/Users/jpc72/AppData/Local/Tesseract-OCR/tesseract.exe'
+
+text_file = open("res.txt", "w")
+
+mypath = "C:/Users/jpc72/Desktop/Vocab Images"
+files = listdir(mypath)
+size = len(files)
+
+#res = ""
+n = 1
+for file_name in files:
+    path = "C:/Users/jpc72/Desktop/Vocab Images/" + file_name
+    img = Image.open(path)
+    text = pytesseract.image_to_string(img, lang='eng')
+    text_file.write(text)
+    print(str(n)+"/"+ str(size))
+    n += 1
+    #res += text
+
+text_file.close()
