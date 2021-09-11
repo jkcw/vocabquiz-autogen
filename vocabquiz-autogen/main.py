f84756c17fc888f606fd165516432ae8d175c56e
JaseprW <jpc72075@gmail.com>
2021-09-12 00:07:08 +0800

    Finished RequestFillInTheBlanksTest

--- vocabquiz-autogen/main.py
+++ vocabquiz-autogen/main.py
@@ -9,22 +9,17 @@ last_vocab = "coffer"
 last_vocab_index = vocab_list.index(last_vocab)
 
 # Load tenses list
-tenses_file = open('tenses-list.json', "rw")
-tenses_list = json.load(tenses_file)
-print(tenses_list)
+tenses_file = open('tenses-list.json', "r")
+tenses_dict = json.load(tenses_file)
 
-d = dict()
-for word in tenses_list:
-    d[word[0]] = word
-tenses_file.write(json.dumps(d))
 
 class RequestVocabData:
     def __init__(self, vocab):
-        self.vocab = vocab
+        self.vocab = vocab.lower()
         self.js_dict = []
         self.part_of_speech = []
 
-    def api_request(self):
+    def http_api_request(self):
         api_request = requests.get('https://api.dictionaryapi.dev/api/v2/entries/en/' + self.vocab)
         js = api_request.json()
         self.js_dict = js[0]
@@ -62,17 +57,55 @@ class RequestFillInTheBlanksTest(RequestVocabData):
         self.blank = "__________________"
 
     def rm_answer_to_blank(self, sentence):
-        sentence.replace(self.vocab, self.blank)
+        # Check if the vocabulary is a verb
+        try:
+            tenses = tenses_dict[self.vocab]
+            for tense in tenses:
+                if tense in sentence:
+                    sentence = sentence.replace(tense, self.blank)
+                    return {"tense": tense, "question": sentence}
+            return {"tense": "error:rm_answer_to_blank", "question": "error:rm_answer_to_blank"}
 
-    def get_def_test(self):
+        except:
+            sentence = sentence.replace(self.vocab, self.blank)
+            return {"tense": self.vocab, "question": sentence}
+
+    def json_to_readable_text(self):
+        # Since there are so much part of speech,
+        # we need to get all part of speech using iteration
         for pos in self.part_of_speech:
             partOfSpeech = pos['partOfSpeech']
 
             # Since there are so many definitions of a part of speech,
             # we need to get all definitions using iteration
             for definition in pos['definitions']:
-                temp_answer = definition['definition']
-                question = definition['example']
+                try:
+                    question = definition['example']
+                except:
+                    continue
+
+                # Remove the answer to blanks
+                removed_ans_dict = self.rm_answer_to_blank(question)
+
+                question = removed_ans_dict['question'] + "\n"
+                temp_answer = removed_ans_dict['tense'] + "/" + partOfSpeech + "/" + definition['definition'] + "\n"
+                self.question += question
+                self.answer += temp_answer
+
+    def get_fill_in_the_blanks_test(self):
+        self.http_api_request()
+        self.get_all_part_of_speech()
+        self.json_to_readable_text()
+        return {
+            'question': self.question,
+            'answer': self.answer
+        }
+
+
+test = RequestFillInTheBlanksTest("get")
+res = test.get_fill_in_the_blanks_test()
+print("Question:\n" + res['question'])
+print("Answer:\n" + res['answer'])
 
 
 
