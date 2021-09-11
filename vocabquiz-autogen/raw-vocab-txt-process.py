ea134082229be5d3d49f25c17d87517df52e2c54
JaseprW <jpc72075@gmail.com>
2021-09-11 19:42:40 +0800

    Removed meaningless vocab

---
+++ vocabquiz-autogen/raw-vocab-txt-process.py
@@ -0,0 +1,19 @@
+import json
+
+
+text_file = open("res.txt", "r")
+res_file = open("res-no-blanks.txt", "w")
+res = []
+
+for vocab in text_file:
+    vocab = vocab.strip("\n")
+    vocab = vocab.strip("\f")
+    print(vocab)
+    if vocab != "" and vocab != "HEB IAL":
+        res.append(vocab)
+
+jsonString = json.dumps(res)
+res_file.write(jsonString)
+
+text_file.close()
+res_file.close()
